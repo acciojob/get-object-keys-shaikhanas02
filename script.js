a1db58881e1712694fdf433e3978cfec5b2d2f0a
shaikhanas02 <122043549+shaikhanas02@users.noreply.github.com>
2023-04-03 14:17:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let student = {
+	name: `Anas`,
+	job: `SDE`,
+	experience: `fresher`
+
+	getKeys(student);
+}
+function getKeys(obj) {
+let keys = Object.keys(obj)
+return keys;
+	
+}
+
+student.getKeys;
